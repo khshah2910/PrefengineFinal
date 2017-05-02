d226c6f56b4fd099c74a6fd47fd138a45aab59f6
khshah2910 <kushshah@10.0.0.23>
2017-05-02 00:33:12 -0400

    Put your own key

--- PrefengineFinal/src/com/prefengine/util/Constants.java
+++ PrefengineFinal/src/com/prefengine/util/Constants.java
@@ -4,7 +4,7 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
 public class Constants {
-	public static final String API_KEY ="AIzaSyAR42Q5Mf0sorfbvOAU2qrM52xXpMarTUo" ;
+	public static final String API_KEY ="" ;
 	public static final String APPLICATION_NAME = "Prefengine";
 	public static final String DB_USERNAME = "root";
 	public static final String DB_PASSWORD = "root";
