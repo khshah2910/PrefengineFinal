c0345d4923260b3cfdf01d1714c546d49abc5f32
khshah2910 <kushshah@10.0.0.23>
2017-05-25 02:33:46 -0400

    Database table creation queries

---
+++ PrefengineFinal/database/tablesCreateStatements/AirlineRanks.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `AirlineRanks` (
+  `Rank` int(11) NOT NULL,
+  `AirlineName` varchar(45) DEFAULT NULL,
+  `AirlineCode` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Rank`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ PrefengineFinal/database/tablesCreateStatements/searchId_seq.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `searchId_seq` (
+  `search_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+insert into searchId_seq (search_id) value (0);
